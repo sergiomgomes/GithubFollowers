5b2cb866b9c95d86eb5e32e014366bb1a61ff1a9
Sergio Gomes (1130399) <1130399@isep.ipp.pt>
2023-03-06 22:47:47 +0000

    Refactor network call using Result type

--- GithubFollowers/Controllers/FollowersViewController.swift
+++ GithubFollowers/Controllers/FollowersViewController.swift
@@ -16,13 +16,13 @@ class FollowersViewController: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
-            guard let followers = followers else {
-                self.presentAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print("Followers = \(followers.count), \(followers)")
+            case .failure(let error):
+                self.presentAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
-            
-            print("Followers = \(followers.count), \(followers)")
         }
     }
     
